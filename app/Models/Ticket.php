4ecba8edc26ffa0839ed1caf500c08572b608db4
XAPHNE <suvnkr.xaphene@gmail.com>
2024-08-19 00:05:27 +0530

    Created Ticket model

---
+++ app/Models/Ticket.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ticket extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $fillable = ['ticket_number', 'created_by', 'closed_by', 'location', 'subject', 'description', 'sla_overdue', 'status', 'remarks', 'closed_at'];
+
+    protected $casts = [
+        'sla_overdue' => 'datetime',
+        'closed_at' => 'datetime',
+    ];
+
+    // Relationship with User as the creator
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    // Relationship with User as the closer
+    public function closer()
+    {
+        return $this->belongsTo(User::class, 'closed_by');
+    }
+}
