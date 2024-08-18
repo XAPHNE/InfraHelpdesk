bbaa4f905174096950261e393f61ad2d61800911
XAPHNE <suvnkr.xaphene@gmail.com>
2024-08-19 00:05:47 +0530

    Created tickets table migration

---
+++ database/migrations/2024_08_18_172354_create_tickets_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('ticket_number');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('closed_by')->nullable();
+            $table->enum('location', ['HQ', 'NTPS', 'LTPS', 'LKHEP', 'KLHEP', 'Longku', 'Narengi', 'Jagiroad']);
+            $table->string('subject');
+            $table->string('description');
+            $table->dateTime('sla_overdue');
+            $table->enum('status', ['Open', 'Closed']);
+            $table->string('remarks')->nullable();
+            $table->dateTime('closed_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Adding foreign key constraints
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('closed_by')->references('id')->on('users')->onDelete('set null'); // You can choose set null or cascade
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
