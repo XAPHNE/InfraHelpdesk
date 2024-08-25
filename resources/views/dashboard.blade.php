fc500970d1c21bbf327b4d10b2de0e6ba7c60f88
XAPHNE <suvnkr.xaphene@gmail.com>
2024-08-25 23:17:41 +0530

    Updated dashboard blade view with boxes for ticket status

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v1</li>
+                        <li class="breadcrumb-item active">Dashboard</li>
                     </ol>
                 </div>
             </div>
@@ -21,20 +21,117 @@
 @endsection
 
 @section('content')
-    <!-- Your content goes here -->
     <div class="row">
+        <!-- Open Tickets in Current Quarter -->
         <div class="col-lg-3 col-6">
             <div class="small-box bg-info">
                 <div class="inner">
-                    <h3>150</h3>
-                    <p>New Orders</p>
+                    <h3>{{ $openTicketsQuarter }}</h3>
+                    <p>Open Tickets (Current Quarter)</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-bag"></i>
+                    <i class="ion ion-clipboard"></i>
                 </div>
                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
-        <!-- Add more boxes here -->
+
+        <!-- Closed Tickets in Current Quarter -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $closedTicketsQuarter }}</h3>
+                    <p>Closed Tickets (Current Quarter)</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-checkmark-circled"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- Open Tickets in Current Month -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $openTicketsMonth }}</h3>
+                    <p>Open Tickets (Current Month)</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-clipboard"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- Closed Tickets in Current Month -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $closedTicketsMonth }}</h3>
+                    <p>Closed Tickets (Current Month)</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-checkmark-circled"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- SLA Overdue Tickets (Current Year) -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-primary">
+                <div class="inner">
+                    <h3>{{ $slaOverdueYear }}</h3>
+                    <p>SLA Overdue (This Year)</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-alert"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- SLA Overdue Tickets (Current Quarter) -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-secondary">
+                <div class="inner">
+                    <h3>{{ $slaOverdueQuarter }}</h3>
+                    <p>SLA Overdue (This Quarter)</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-alert"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- SLA Overdue Tickets (Current Month) -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $slaOverdueMonth }}</h3>
+                    <p>SLA Overdue (This Month)</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-alert"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- SLA Overdue Tickets (Last Month) -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $slaOverdueLastMonth }}</h3>
+                    <p>SLA Overdue (Last Month)</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-alert"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
     </div>
 @endsection
